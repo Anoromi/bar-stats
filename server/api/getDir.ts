9f8ce0ad0dd1de6f782b890fadc0dbd437163e58
Anoromi <and.zagorulko@gmail.com>
2024-08-07 17:51:01 +0300

    Test 9

--- server/api/getDir.ts
+++ server/api/getDir.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async () => {
   return {
     a: readdirSync('./chunks/raw'),
     mount: useStorage('assets:drizzle').getMount().base,
-    drizzle: await useStorage('assets:drizzle').getKeys()
+    drizzle: await useStorage('assets:drizzle').getKeys(),
     drizzle2: await useStorage('assets:drizzle2').getKeys()
   }
     
