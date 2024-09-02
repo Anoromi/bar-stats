d89b79cd4c2422cd1d2b3a4eab50eb8916a726fc
Anoromi <and.zagorulko@gmail.com>
2024-09-02 11:26:39 +0300

    add dtos

---
+++ server/utils/dto/dto.ts
@@ -0,0 +1,15 @@
+import type { BattleEntity, BattleEntityInsert, BattleTeamEntityInsert, MapEntity, MapEntityInsert, UserToBattleTeamEntity, UserToBattleTeamEntityInsert } from "../database/schema";
+
+export type MapDto = MapEntity
+export type MapDtoInsert = MapEntityInsert
+
+export type BattleDto = BattleEntity
+export type BattleDtoInsert = BattleEntityInsert
+
+export type BattleTeamDto = BattleTeamEntity
+export type BattleTeamDtoInsert = BattleTeamEntityInsert
+
+export type UserToBattleTeamDto = UserToBattleTeamEntity
+export type UserToBattleTeamDtoInsert = UserToBattleTeamEntityInsert
+
+
