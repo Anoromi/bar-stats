2282a03199143bd265e5fe2e52eb0d13bb7c7af5
Anoromi <and.zagorulko@gmail.com>
2024-09-02 11:16:12 +0300

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
+import prettierConfig from "eslint-config-prettier";
+
+console.log(prettierConfig)
+export default createConfigForNuxt({}).append(
+  ...[
+    {
+      plugins: {
+        "unused-imports": pluginUnusedImports,
+      },
+    },
+    {
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "warn",
+        "unused-imports/no-unused-vars": [
+          "warn",
+          {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+          },
+        ],
+      },
+    },
+    prettierConfig,
+
+    // {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+    // pluginJs.configs.recommended,
+    // ...tseslint.configs.recommended,
+    // ...pluginVue.configs["flat/essential"],
+    // {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  ],
+);
