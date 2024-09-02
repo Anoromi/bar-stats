ad9ac6aad9f19cfacc70b08456352f2e823541cf
Anoromi <and.zagorulko@gmail.com>
2024-09-02 11:24:44 +0300

    cleanup plugins

--- server/plugins/database-plugin.ts
+++ server/plugins/database-plugin.ts
@@ -1,41 +1,15 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import { updateDb } from "../utils/database";
-import { migrate } from "drizzle-orm/libsql/migrator";
-import { readdir } from "node:fs"
+import * as schema from '../utils/database/schema'
+import { and, eq, sql } from "drizzle-orm";
+import { parse } from "vue/compiler-sfc";
+import { SQLiteAsyncDialect, SQLiteDialect } from "drizzle-orm/sqlite-core";
 
 
 export default defineNitroPlugin(async (nitro) => {
   const {DB_AUTH_TOKEN, DB_URL} = useRuntimeConfig()
   const client = createClient({ url:DB_URL, authToken: DB_AUTH_TOKEN });
 
-  
-  
-
-  
-
-  //const db = drizzle(client);
-  //console.log(db)
-
-
-  updateDb(drizzle(client))
-
-  //await migrate(db, {migrationsFolder: 'assets:server'})
-
-
-
-
-
-  //console.log(db)
-
-  //db.insert(countries).values({
-  //  name: 'hehe hehe'
-  //}).then(() => {
-  //  console.log('ok')
-  //}, error => {
-  //  console.log('error', error)
-  //})
-
-  //const result = await db.select().from(users).all()
-
+  updateDb(drizzle(client, {schema}))
 })
