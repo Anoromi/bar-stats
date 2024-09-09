f8d12e505d975e817d5a8e7dfba46a1f22c1c508
Anoromi <and.zagorulko@gmail.com>
2024-09-09 12:15:49 +0300

    update database init

--- server/utils/database.ts
+++ server/utils/database.ts
@@ -1,8 +1,20 @@
-import { LibSQLDatabase } from "drizzle-orm/libsql";
+import { drizzle, type LibSQLDatabase } from "drizzle-orm/libsql";
 import * as schema from "./database/schema";
+import { createClient } from "@libsql/client";
+import consola from "consola";
 
-export let db: LibSQLDatabase<typeof schema>;
+// eslint-disable-next-line import/no-mutable-exports
+export let db: LibSQLDatabase<typeof schema> =
+  null as unknown as LibSQLDatabase<typeof schema>;
 
-export function updateDb(newDb: LibSQLDatabase<typeof schema>) {
-  db = newDb;
+export function initDb(url: string, authToken?: string) {
+  consola.log("checking db");
+  if (db === null) {
+    consola.log("initializing db");
+    consola.log('url', url, authToken)
+    const client = createClient({ url: url, authToken: authToken });
+    db = drizzle(client, { schema });
+    consola.log('select', db, drizzle(client, { schema }))
+  }
+  return db;
 }
