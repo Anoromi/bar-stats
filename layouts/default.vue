5aa0505be0f9395c4e5c6fe8dd513cd5e0c44e00
Anoromi <and.zagorulko@gmail.com>
2024-09-09 12:13:26 +0300

    add overview link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,16 @@
 <template>
   <main class="flex flex-col overflow-auto h-screen">
     <header
-      class="h-16 bg-surface text-surface-foreground flex items-center px-2 sm:px-4"
+      class="h-16 py-4 bg-surface text-surface-foreground flex items-baseline px-2 sm:px-4"
     >
       <NuxtLink class="text-primary text-2xl" to="/">
         <b>BAR</b> Stats
       </NuxtLink>
+      <nav class="ml-12">
+      <NuxtLink class="text-surface-foreground" to="/general">
+        overview
+      </NuxtLink>
+      </nav>
     </header>
     <slot></slot>
   </main>
