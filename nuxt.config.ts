6aebfa9334d5547245095b2bfd74f6d97de4558d
Anoromi <and.zagorulko@gmail.com>
2024-09-09 12:50:53 +0300

    switch to lpostbuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-    "@nuxt/fonts",
-    "@nuxthub/core",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/fonts"],
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
