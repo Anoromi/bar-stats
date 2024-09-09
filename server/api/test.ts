672677431a59269ffe6af5b7807a4bb53d5277cd
Anoromi <and.zagorulko@gmail.com>
2024-09-09 12:14:37 +0300

    update test route

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,25 +1,25 @@
 import * as z from "zod";
-import consola from "consola";
-import { getMapsByMapIdQuery } from "../utils/services/mapService";
 
+// eslint-disable-next-line unused-imports/no-unused-vars
 const requestQuery = z.object({
   user: z.string().optional(),
 });
 
 export type TestRequestQuery = z.infer<typeof requestQuery>;
 
-export default defineEventHandler(async (event) => {
-  const replayId = "d0e1c466087b6c51339fd86cd55b4431";
-
-  const query = getMapsByMapIdQuery([23, 23]);
-  //new SQLiteAsyncDialect()
-
-  //new SQL(query.toSQL)
-
-  consola.log(query.toSQL());
+export default defineEventHandler(async () => {
+  const mapService = useMapService()
+  return mapService.getMapsByMapId([549])
+  // const replayId = "d0e1c466087b6c51339fd86cd55b4431";
+  //
+  // const query = getMapsByMapIdQuery([23, 23]);
+  // //new SQLiteAsyncDialect()
+  //
+  // //new SQL(query.toSQL)
+  //
+  //consola.log(query.toSQL());
   //const explanation = await db.run(sql`EXPLAIN QUERY PLAN ${query}`)
   //consola.log(explanation)
-
   //const request = await getBarReplay(replayId);
 });
 
