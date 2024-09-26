f3c6c4382b7db16927c0607d038536f1187267ef
Anoromi <and.zagorulko@gmail.com>
2024-09-26 09:45:33 +0300

    restyle general page

--- pages/general/index.vue
+++ pages/general/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="tsx">
-import consola from "consola";
 import { toTypedSchema } from "@vee-validate/zod";
 import * as z from "zod";
 import { useForm } from "vee-validate";
-import type { BattleProcessorParams } from "~/utils/worker/battlesProcessor";
+import type {
+  BattlesProcessorRequest,
+  BattlesProcessorResponse,
+} from "~/utils/worker/battlesProcessorWorker";
+import { useClientWorker } from "~/utils/worker/useClientWorker";
+import { toast } from "~/components/ui/toast";
 
 const formSchema = toTypedSchema(
   z.object({
@@ -15,7 +19,7 @@ const formSchema = toTypedSchema(
       })
       .array()
       .default([]),
-    map: z.string(),
+    map: z.string().optional(),
   }),
 );
 
@@ -23,46 +27,81 @@ const form = useForm({
   validationSchema: formSchema,
 });
 
-watchEffect(async () => {
-  const client = import.meta.client;
-  if (client) {
-    const hehe2 = new Worker(
-      new URL("~/utils/worker/battlesProcessor", import.meta.url),
-      {
-        type: "module",
-      },
-    );
-    hehe2.postMessage("eheheheh");
-    hehe2.postMessage({
-      type: "battle",
-      requestParams: {
-        map: null,
-        users: null,
-        limit: null,
-        battleType: null,
-      },
-    } satisfies BattleProcessorParams);
-    consola.log("loaded", hehe2);
-    hehe2.onmessage = (event) => {
-      console.log(event);
-    };
-  }
-});
+const { worker } = useClientWorker<
+  BattlesProcessorRequest,
+  BattlesProcessorResponse
+>(
+  () =>
+    new Worker(
+      new URL("~/utils/worker/battlesProcessorWorker", import.meta.url),
+      { type: "module" },
+    ),
+);
+
+//watchEffect(async () => {
+//  const client = import.meta.client;
+//  if (client) {
+//    const hehe2 = new Worker(
+//      new URL("~/utils/worker/battlesProcessorWorker", import.meta.url),
+//      {
+//        type: "module",
+//      },
+//    );
+//    hehe2.postMessage("eheheheh");
+//    hehe2.postMessage({
+//      type: "battle",
+//      requestParams: {
+//        map: null,
+//        users: null,
+//        limit: null,
+//        battleType: null,
+//      },
+//    } satisfies BattleProcessorParams);
+//    consola.log("loaded", hehe2);
+//    hehe2.onmessage = (event) => {
+//      console.log(event);
+//    };
+//  }
+//});
 
 const onSubmit = form.handleSubmit((values) => {
   console.log("Form submitted!", values);
+  toast({
+    title: "Form submitted!",
+    description: JSON.stringify(values),
+  });
+  const { map, users } = form.values;
+
+  worker
+    .value!.request({
+      type: "battle",
+      params: {
+        battleType: null,
+        map: map ?? null,
+        users: users?.flatMap((v) => v.id) ?? null,
+        limit: 500,
+      },
+    })
+    .then((v) => {
+      console.log("worker result is ", v);
+    });
 });
 </script>
 
 <template>
-  <article class="flex-col flex items-center p-4">
-    <form class="w-full sm:w-[64rem] sm:mt-10 mt-4" @submit="onSubmit">
-      <legend class="mb-4 text-lg font-bold">
-        Specify data for search
-      </legend>
-      <GeneralMapSelector name="map"></GeneralMapSelector>
-      <GeneralUserSelector name="users"></GeneralUserSelector>
-      <Button type="submit">Submit</Button>
-    </form>
+  <article class="flex flex-1 flex-col items-center p-4">
+    <div class="flex w-full flex-1 flex-col md:flex-row md:justify-center">
+      <form
+        class="mb-4 mt-4 flex h-max flex-col gap-y-4 rounded-2xl bg-surface p-4 sm:mt-10 md:w-96 md:shadow-md"
+        @submit="onSubmit"
+      >
+        <legend class="mb-2 text-lg font-bold">Filter</legend>
+        <GeneralMapSelector name="map"></GeneralMapSelector>
+        <GeneralUserSelector name="users"></GeneralUserSelector>
+        <Button type="submit" variant="outline">Update</Button>
+      </form>
+
+      <div class="flex-1 md:max-w-[56rem]">Other content ehe</div>
+    </div>
   </article>
 </template>
