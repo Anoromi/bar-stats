767be060dfbdba069fd24c82e31086ae26b7b9fa
Anoromi <and.zagorulko@gmail.com>
2024-09-27 12:09:02 +0300

    change runetime config to environment variables

--- server/plugins/database-plugin.ts
+++ server/plugins/database-plugin.ts
@@ -2,7 +2,8 @@ import { initDb } from "../utils/database";
 
 export default defineNitroPlugin(async (nitro) => {
   const { dbURL, dbAuthToken } = useRuntimeConfig();
+  console.log('process', process.env.DB_URL, process.env.DB_AUTH_TOKEN);
   nitro.hooks.hook("request", () => {
-    initDb(dbURL, dbAuthToken);
+    initDb(process.env.DB_URL!, process.env.DB_AUTH_TOKEN!);
   });
 });
