c5396eecda16004509b2d060c9a0b16f36533344
Anoromi <and.zagorulko@gmail.com>
2024-10-02 12:13:24 +0300

    add average os to time chart

---
+++ components/General/AverageOsToTimeChart.vue
@@ -0,0 +1,34 @@
+<script setup lang="tsx">
+const props = defineProps<{
+  data: [os: number, time: number][];
+}>();
+
+const option = computed<ECOption>(() => ({
+  title: {
+    text: "Avg os to time",
+  },
+  xAxis: {
+    type: "value",
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: props.data,
+      type: "line",
+      //color: ["red", "blue"],
+      //colorBy: "data",
+      itemStyle: {
+        borderRadius: [8, 8, 0, 0],
+      },
+      showSymbol: false,
+      
+    },
+  ],
+}));
+</script>
+
+<template>
+  <VChart :option="option" :init-options="{ height: 400 }" class="h-[400px]"></VChart>
+</template>
