5bcacb5540c73d9e1710b1631942747c390da7d2
Anoromi <and.zagorulko@gmail.com>
2024-10-09 18:53:07 +0300

    enhance database battle query

--- server/utils/services/battleService.ts
+++ server/utils/services/battleService.ts
@@ -1,5 +1,15 @@
-import type { SQLWrapper } from "drizzle-orm";
-import { and, count, desc, eq, exists, inArray, max, sql } from "drizzle-orm";
+import type { SQL, SQLWrapper } from "drizzle-orm";
+import {
+  and,
+  count,
+  desc,
+  eq,
+  exists,
+  inArray,
+  lt,
+  max,
+  sql,
+} from "drizzle-orm";
 import type { BattleEntity } from "../database/schema";
 import { battleTable } from "../database/schema";
 import consola from "consola";
@@ -10,7 +20,6 @@ import type {
   UserToBattleTeamDto,
 } from "../dto/dto";
 import type { MapService } from "#imports";
-import { createClient } from "@libsql/client";
 
 export const lastBattleQuery = () =>
   db
@@ -49,6 +58,52 @@ export class BattleService {
       .values(battle)
       .returning();
     return result[0];
+  }
+
+  private async getBattlesAfter(
+    date: Date | null,
+    whereClause: SQL<unknown>,
+    limit: number,
+  ) {
+    const battleIds = db
+      .selectDistinct({
+        id: battleTable.id,
+      })
+      .from(battleTable)
+      .where(
+        and(
+          whereClause,
+          ...(date !== null ? [lt(battleTable.startTime, new Date(date))] : []),
+        ),
+      )
+      .innerJoin(
+        userToBattleTable,
+        eq(battleTable.id, userToBattleTable.battleTeamBattleId),
+      )
+      .limit(limit)
+      .orderBy(desc(battleTable.startTime));
+
+    //console.log("ids", await battleIds);
+    const battles = await logAnalyze(
+      db
+        .select({
+          battle: battleTable,
+          player: userToBattleTable,
+        })
+        .from(battleTable)
+        .where(
+          and(
+            eq(userToBattleTable.isSpectator, false),
+            inArray(battleTable.id, battleIds),
+          ),
+        )
+        .innerJoin(
+          userToBattleTable,
+          eq(battleTable.id, userToBattleTable.battleTeamBattleId),
+        )
+        .orderBy(desc(battleTable.startTime)),
+    );
+    return battles;
   }
 
   async getBattles(
@@ -111,204 +166,41 @@ export class BattleService {
     }
 
     console.count("battle");
+    const whereClause = and(
+      eq(battleTable.endedNormally, true),
+      eq(battleTable.hasBots, false),
+      eq(userToBattleTable.isSpectator, false),
+      ...conditions,
+      ...(battleType != null ? [eq(battleTable.battleType, battleType)] : []),
+    );
 
-    const battleIds = db
-      .selectDistinct({
-        id: battleTable.id,
-      })
-      .from(battleTable)
-      .where(
-        and(
-          ...conditions,
-          eq(userToBattleTable.isSpectator, false),
-          eq(battleTable.endedNormally, true),
-          eq(battleTable.hasBots, false),
-          ...(battleType != null
-            ? [eq(battleTable.battleType, battleType)]
-            : []),
-        ),
-      )
-      .innerJoin(
-        userToBattleTable,
-        eq(battleTable.id, userToBattleTable.battleTeamBattleId),
-      )
-      .limit(limit)
-      .orderBy(desc(battleTable.startTime));
+    const battles: BattleWithPlayers[] = [];
+    let leftElements = limit;
+    let currentLastBattle: Date | null = null;
+    while (leftElements > 0) {
+      const take = leftElements > 500 ? 500 : leftElements;
+      leftElements -= take;
 
-    //console.log("ids", await battleIds);
-    const battles = await db
-      .select({
-        battle: battleTable,
-        player: userToBattleTable,
-      })
-      .from(battleTable)
-      .where(
-        and(
-          eq(userToBattleTable.isSpectator, false),
-          inArray(battleTable.id, battleIds),
-        ),
-      )
-      .innerJoin(
-        userToBattleTable,
-        eq(battleTable.id, userToBattleTable.battleTeamBattleId),
-      )
-      .orderBy(desc(battleTable.startTime));
-    //const { DB_AUTH_TOKEN, DB_URL } = useRuntimeConfig();
-    //const client = createClient({ url: DB_URL, authToken: DB_AUTH_TOKEN });
-    //const query = battles.toSQL();
-//    try {
-//      const result = await client.execute({
-//        sql: `
-//SELECT
-//  "battle"."id",
-//  "battle"."engine-version",
-//  --"battle"."map-id",
-//  --"battle"."game-version",
-//  --"battle"."start-time",
-//  --"battle"."duration-ms",
-//  --"battle"."full-duration-ms",
-//  --"battle"."winning-team",
-//  --"battle"."has-bots",
-//  --"battle"."ended-normally",
-//  --"battle"."player-count",
-//  --"battle"."battle-type",
-//  --"battle"."preset",
-//  "user-to-battle"."user-username"
-//  --"user-to-battle"."user-id",
-//  --"user-to-battle"."battle-team-battle-id",
-//  --"user-to-battle"."battle-team-number",
-//  --"user-to-battle"."skill",
-//  --"user-to-battle"."skill-uncertainty",
-//  --"user-to-battle"."rank",
-//  --"user-to-battle"."is-spectator",
-//  --"user-to-battle"."player-id",
-//  --"user-to-battle"."team-id",
-//  --"user-to-battle"."faction",
-//  --"user-to-battle"."start-pos-x",
-//  --"user-to-battle"."start-pos-y",
-//  --"user-to-battle"."start-pos-z"
-//FROM
-//  "battle"
-//  INNER JOIN "user-to-battle" ON "battle"."id" = "user-to-battle"."battle-team-battle-id"
-//WHERE
-//  (
-//    "user-to-battle"."is-spectator" = 0
-//    AND "battle"."id" IN (
-//      SELECT DISTINCT
-//        "battle"."id"
-//      FROM
-//        "battle"
-//        INNER JOIN "user-to-battle" ON "battle"."id" = "user-to-battle"."battle-team-battle-id"
-//      WHERE
-//        (
-//          "user-to-battle"."is-spectator" = 0
-//          AND "battle"."ended-normally" = 1
-//          AND "battle"."has-bots" = 0
-//        )
-//      ORDER BY
-//        "battle"."start-time" DESC
-//      LIMIT
-//        10
-//    )
-//  )
-//ORDER BY
-//  "battle"."start-time" DESC
-//      `,
-//         
-//        args: [],
-//      });
-//
-//      console.log("result", result);
-//    } catch (e) {
-//      console.log(e);
-//    }
-    //const result = await db.run(battles.getSQL());
+      const next = await this.getBattlesAfter(
+        currentLastBattle,
+        whereClause!,
+        take,
+      );
 
-    //throw new Error();
-    // console.count("battle");
-    // const battleRequests = battleIds.map((v) =>
-    //   db
-    //     .select({
-    //       battle: battleTable,
-    //       player: userToBattleTable,
-    //     })
-    //     .from(battleTable)
-    //     .where(
-    //       and(
-    //         eq(battleTable.id, v.id),
-    //         eq(userToBattleTable.isSpectator, false),
-    //       ),
-    //     )
-    //     .innerJoin(
-    //       userToBattleTable,
-    //       eq(battleTable.id, userToBattleTable.battleTeamBattleId),
-    //     )
-    //     .limit(1)
-    //     .orderBy(desc(battleTable.startTime)),
-    // );
-    //
-    // type BattleRequestsResult = Awaited<(typeof battleRequests)[number]>;
-    //
-    // console.count("battle");
-    // console.log("count", battleRequests.length);
-    //
-    // const q = [db.select().from(battleTable)];
-    //
-    // const battle = (
-    //   (await db.batch(battleRequests as any)) as BattleRequestsResult[]
-    // ).map((v) => {
-    //   console.log(v);
-    //   return {
-    //     key: v[0].battle,
-    //     values: v.map((x) => x.player),
-    //   } satisfies BattleWithPlayers;
-    // });
-    console.count("battle");
-    //const k = [
-    //  db.select().from(battleTable)
-    //] as const
-    //db.batch(k)
-    //db.batch
-    // db.batch(
-    //     k
-    //   // battleIds.map((v) =>
-    //   //   db
-    //   //     .select({
-    //   //       battle: battleTable,
-    //   //       player: userToBattleTable,
-    //   //     })
-    //   //     .from(battleTable)
-    //   //     .where(eq(battleTable.id, v.id))
-    //   //     .innerJoin(
-    //   //       userToBattleTable,
-    //   //       eq(battleTable.id, userToBattleTable.battleTeamBattleId),
-    //   //     )
-    //   //     .limit(limit)
-    //   //     .orderBy(desc(battleTable.startTime)),
-    //   // ),
-    // );
-    //const battle = await logAnalyze(
-    //  db
-    //    .select({
-    //      battle: battleTable,
-    //      player: userToBattleTable,
-    //    })
-    //    .from(battleTable)
-    //    .where()
-    //    .innerJoin(
-    //      userToBattleTable,
-    //      eq(battleTable.id, userToBattleTable.battleTeamBattleId),
-    //    )
-    //    .limit(limit)
-    //    .orderBy(desc(battleTable.startTime)),
-    //);
+      if(next.length === 0)
+        break;
 
-    return groupByMappedWithMap(battles, {
-      selectGroupKey: (value) => value.battle,
-      selectGroupValue: (value) => value.player,
-      getMappableKey: (group) => group.id,
-    });
-    //return battle;
+      const grouped = groupByMappedWithMap(next, {
+        selectGroupKey: (value) => value.battle,
+        selectGroupValue: (value) => value.player,
+        getMappableKey: (group) => group.id,
+      });
+      console.log('normal vs grouped', next.length, grouped.length, take, leftElements, next.at(-1), grouped.at(-1));
+      currentLastBattle = grouped.at(-1)!.key.startTime;
+      battles.push(...grouped);
+    }
+
+    return battles;
   }
 
   insertBattles() {}
