c0537a5ba367eca13ca0229eb552ea07a3f8b393
Anoromi <and.zagorulko@gmail.com>
2024-10-11 17:23:24 +0300

    add cluster test chart

--- pages/general/index.vue
+++ pages/general/index.vue
@@ -8,7 +8,7 @@ import type {
 } from "~/utils/battleProcessor/worker";
 import { useClientWorker } from "~/utils/worker/useClientWorker";
 import { toast } from "~/components/ui/toast";
-import type { StackedBarDataRecord } from "@unovis/ts/components/stacked-bar/types";
+
 
 const formSchema = toTypedSchema(
   z.object({
@@ -33,16 +33,12 @@ const { worker } = useClientWorker<
   BattlesProcessorResponse
 >(
   () =>
-    new Worker(
-      new URL("~/utils/battleProcessor/worker", import.meta.url),
-      { type: "module" },
-    ),
+    new Worker(new URL("~/utils/battleProcessor/worker", import.meta.url), {
+      type: "module",
+    }),
 );
 
 const results = ref<BattlesProcessorResponse>();
-
-
-
 
 const onSubmit = form.handleSubmit((values) => {
   console.log("Form submitted!", values);
@@ -59,7 +55,7 @@ const onSubmit = form.handleSubmit((values) => {
         battleType: null,
         map: map ?? null,
         users: users?.flatMap((v) => v.id) ?? null,
-        limit: 1000,
+        limit: 3000,
       },
     })
     .then((v) => {
@@ -67,32 +63,29 @@ const onSubmit = form.handleSubmit((values) => {
       results.value = v;
     });
 });
-
-
-const color = (d: StackedBarDataRecord<unknown>, i: number) =>
-  ["#04c0c7", "#5144d3", "#da348f"][i];
 </script>
 
 <template>
   <article class="flex flex-1 flex-col items-center p-4">
     <div class="flex w-full flex-1 flex-col xl:flex-row xl:justify-center">
       <form
-        class="mb-4 mt-4 flex h-max flex-col gap-y-4 md:max-w-96 rounded-2xl xl:bg-surface p-4 sm:mt-10 xl:w-96 xl:shadow-md"
-        @submit="onSubmit"
-      >
+        class="mb-4 mt-4 flex h-max flex-col gap-y-4 rounded-2xl p-4 sm:mt-10 md:max-w-96 xl:w-96 xl:bg-surface xl:shadow-md"
+        @submit="onSubmit">
         <legend class="mb-2 text-lg font-bold">Filter</legend>
         <GeneralMapSelector name="map"></GeneralMapSelector>
         <GeneralUserSelector name="users"></GeneralUserSelector>
         <Button type="submit" variant="outline">Update</Button>
       </form>
 
-      <div class="flex-1 min-w-0 xl:max-w-[56rem] py-10 xl:pl-20 pr-5">
-
+      <div class="min-w-0 flex-1 py-10 pr-5 xl:max-w-[56rem] xl:pl-20">
+        <GeneralClusterTest></GeneralClusterTest>
         <template v-if="results !== undefined">
-          <LazyGeneralWinrateChart  :data="results.data.factionWinrate"></LazyGeneralWinrateChart>
-          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime" ></LazyGeneralAverageOsToTimeChart>
-          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime2" ></LazyGeneralAverageOsToTimeChart>
-          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime3" ></LazyGeneralAverageOsToTimeChart>
+          <LazyGeneralMapPoints v-if="results.data.clusteredData !== undefined" :data="results.data.clusteredData!"
+            :map="results.data.mapName!"></LazyGeneralMapPoints>
+          <LazyGeneralWinrateChart :data="results.data.factionWinrate"></LazyGeneralWinrateChart>
+          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime"></LazyGeneralAverageOsToTimeChart>
+          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime2"></LazyGeneralAverageOsToTimeChart>
+          <LazyGeneralAverageOsToTimeChart :data="results.data.osToTime3"></LazyGeneralAverageOsToTimeChart>
           <!-- <BarChart -->
           <!--   :data="factionWinrate!" -->
           <!--   index="name" -->
