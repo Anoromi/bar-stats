f2580987608595d7ca976ffeeeb72fb308866eed
Anoromi <and.zagorulko@gmail.com>
2024-10-16 21:38:24 +0300

    update faction winrate chart

--- components/General/WinrateChart.vue
+++ components/General/WinrateChart.vue
@@ -5,9 +5,13 @@ const props = defineProps<{
 
 const factionWinrate = computed(() => {
   const winrate = props.data;
-  return Object.keys(winrate).map((faction) => {
-    return { name: faction, ratio: winrate[faction] };
-  }).filter(v => v.name === 'Random');
+  console.log("winrate 1", winrate);
+  return Object.keys(winrate)
+    .map((faction) => {
+      console.log("faction", faction);
+      return { name: faction, ratio: winrate[faction] };
+    })
+    .filter((v) => v.name !== "Random");
 });
 
 //const canvas = useTemplateRef<ComponentPublicInstance>("winrate-canvas");
@@ -21,24 +25,41 @@ const factionWinrate = computed(() => {
 //  primaryColor.value = canvasStyles.getPropertyValue("--primary");
 //});
 
+function getFactionName(name: string) {
+  switch (name) {
+    case "Armada":
+      return "Armada";
+    case "Cortex":
+      return "cortex";
+    case "Unknown":
+      return "Legion";
+    default:
+      throw new Error("Weird faction name " + name);
+  }
+}
+
 const option = computed<ECOption>(() => {
+  console.log("winrates", factionWinrate.value);
   const colors = factionWinrate.value.map((v) => {
     switch (v.name) {
       case "Armada":
         return "blue";
       case "Cortex":
         return "red";
+      case "Unknown":
+        return "green";
       default:
-        throw new Error("Weird faction name" + v.name);
+        throw new Error("Weird faction name " + v.name);
     }
   });
+
   return {
     title: {
-      text: "Faction win rate",
+      text: "Faction win factor",
     },
     xAxis: {
       type: "category",
-      data: factionWinrate.value.map((v) => v.name),
+      data: factionWinrate.value.map((v) => getFactionName(v.name)),
     },
     yAxis: {
       type: "value",
@@ -59,7 +80,12 @@ const option = computed<ECOption>(() => {
 </script>
 
 <template>
-  <VChart ref="winrate-canvas" :option="option" :init-options="{
-    height: 400,
-  }" class="h-[400px]"></VChart>
+  <VChart
+    ref="winrate-canvas"
+    :option="option"
+    :init-options="{
+      height: 400,
+    }"
+    class="h-[400px]"
+  ></VChart>
 </template>
