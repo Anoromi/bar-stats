014dcd5e1a0d16acda978b223a100d491b07a7a8
Anoromi <and.zagorulko@gmail.com>
2024-10-16 21:32:22 +0300

    abstract osToTime

--- utils/battleProcessor/osToTime.ts
+++ utils/battleProcessor/osToTime.ts
@@ -1,25 +1,67 @@
+import type { BattleWithPlayers } from "~/server/utils/services/battleService";
+
 export function calculateAvgOsToTime(
   battles: BattleWithPlayers[],
-  meanSize: number = 100
+  meanSize: number = 100,
 ): [os: number, time: number][] {
+  return calculateParamToTime(battles, meanSize, (v) => v.key.averageOs!);
+}
+
+function minDiff(battles: BattleWithPlayers) {
+  let minSkill = battles.values[0].skill!;
+  let maxSkill = battles.values[0].skill!;
+  for (let i = 1; i < battles.values.length; i++) {
+    const playerSkill = battles.values[i].skill!;
+    if (playerSkill > maxSkill!) {
+      maxSkill = playerSkill;
+    }
+    if (playerSkill < minSkill!) {
+      minSkill = playerSkill;
+    }
+  }
+  //console.log('skill diff', battles, maxSkill, minSkill)
+  return maxSkill - minSkill;
+}
+
+export function calculateOsDiffToTime(
+  battles: BattleWithPlayers[],
+  meanSize: number = 100,
+): [os: number, time: number][] {
+  return calculateParamToTime(battles, meanSize, minDiff);
+}
+
+function calculateParamToTime(
+  battles: BattleWithPlayers[],
+  meanSize: number = 100,
+  extractValue: (a: BattleWithPlayers) => number | null,
+) {
   const sortedBattles = battles.sort((a, b) => {
-    return (a.key.averageOs ?? 0) - (b.key.averageOs ?? 0);
+    const va = extractValue(a);
+    const vb = extractValue(b);
+    if (va === null && vb === null) return 0;
+    if (va === null) return -1;
+    if (vb === null) return 1;
+    return va - vb;
   });
 
   const osArr: number[] = [];
   const timeArr: number[] = [];
 
   for (let i = 0; i < sortedBattles.length; i++) {
-    const value = sortedBattles[i];
-    if (value.key.averageOs !== null) {
-      osArr.push(value.key.averageOs);
-      timeArr.push(value.key.durationMs / 1000 / 60);
+    const battle = sortedBattles[i];
+    const value = extractValue(battle);
+    if (value !== null) {
+      osArr.push(value);
+      timeArr.push(battle.key.durationMs / 1000 / 60);
     }
   }
 
+  console.log(sortedBattles);
+  console.log(timeArr);
 
   const smoothedTimeArr = movingMean(timeArr, meanSize);
-
+  console.log('smoothed', smoothedTimeArr);
+  console.log('osArr', osArr);
 
   const results: [number, number][] = [];
 
@@ -27,13 +69,16 @@ export function calculateAvgOsToTime(
   let lastTimeSum: number | null = null;
   let lastTimeCount: number | null = null;
   for (let i = 0; i < osArr.length; i++) {
+    //console.log(i)
     const os = osArr[i];
     const smoothedTime = smoothedTimeArr[i];
     if (lastOs !== null && os === lastOs) {
+      //console.log('adding', os, lastOs)
       lastTimeSum! += smoothedTime;
       lastTimeCount! += 1;
       continue;
-    } else if (lastOs !== null) {
+    } 
+    if (lastOs !== null) {
       results.push([lastOs, lastTimeSum! / lastTimeCount!]);
     }
 
@@ -41,6 +86,7 @@ export function calculateAvgOsToTime(
     lastTimeSum = smoothedTime;
     lastTimeCount = 1;
   }
+  console.log('results', results)
   return results;
 }
 
@@ -57,7 +103,7 @@ function movingMean(values: number[], meanSize = 100) {
   const smoothedValues: number[] = [];
 
   for (; i < meanSize / 2 + 1; i++) {
-    const lastValueIndex = Math.floor(meanSize / 2) + i + 1
+    const lastValueIndex = Math.floor(meanSize / 2) + i + 1;
     sum += values[lastValueIndex];
     smoothedValues.push(sum / lastValueIndex);
   }
@@ -69,7 +115,9 @@ function movingMean(values: number[], meanSize = 100) {
   }
   for (; i < values.length; i++) {
     sum -= values[i - Math.floor(meanSize / 2) - 1];
-    smoothedValues.push(sum / (Math.floor(meanSize / 2) + (values.length - i - 1) + 1));
+    smoothedValues.push(
+      sum / (Math.floor(meanSize / 2) + (values.length - i - 1) + 1),
+    );
   }
 
   return smoothedValues;
