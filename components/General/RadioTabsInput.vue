dbc744ef9248f91fdc55672357305a11bdb43e4e
Anoromi <and.zagorulko@gmail.com>
2024-10-20 20:55:35 +0300

    add radio tabs input

---
+++ components/General/RadioTabsInput.vue
@@ -0,0 +1,37 @@
+<script setup lang="tsx">
+import { RadioGroupRoot, RadioGroupItem } from "radix-vue";
+import { useField } from "vee-validate";
+
+const props = defineProps<{
+  name: string;
+  values: {value: string | undefined, label: string}[];
+  label: string;
+}>();
+
+const formField = useField<string | undefined>(props.name);
+</script>
+<template>
+  <div>
+    <fieldset>
+      <Label as="legend" class="mb-2"> {{props.label}} </Label>
+
+      <RadioGroupRoot
+        class="flex flex-col gap-2.5"
+        :model-value="formField.value.value"
+        aria-label="View density"
+        @update:model-value="(v) => formField.setValue(v)"
+      >
+        <div class="flex items-center space-x-4">
+          <RadioGroupItem
+            v-for="v in props.values"
+            :key="v.label"
+            class="py-2, rounded-[0.6rem] border border-solid border-foreground/80 px-3 text-surface-foreground transition-colors hover:bg-primary/20 active:bg-primary/10 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+            :value="v.value"
+          >
+            {{ v.label }}
+          </RadioGroupItem>
+        </div>
+      </RadioGroupRoot>
+    </fieldset>
+  </div>
+</template>
