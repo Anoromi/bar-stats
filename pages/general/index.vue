d805d62efc734a3a33be99de1f27c8d5f68b5b1c
Anoromi <and.zagorulko@gmail.com>
2024-10-20 20:55:08 +0300

    update general page form params

--- pages/general/index.vue
+++ pages/general/index.vue
@@ -9,6 +9,9 @@ import type {
 import { useClientWorker } from "~/utils/worker/useClientWorker";
 import { toast } from "~/components/ui/toast";
 
+const allowedDataLimits = ["500", "1000", "5000"] as const satisfies unknown[];
+const allowedOsOptions = ["<=20", ">=20"] as const satisfies unknown[];
+
 const formSchema = toTypedSchema(
   z.object({
     users: z
@@ -20,9 +23,10 @@ const formSchema = toTypedSchema(
       .array()
       .default([]),
     map: z.string().optional(),
-    limit: z.enum(["500", "1000", "3000"]).default("500"),
+    limit: z.enum(allowedDataLimits).default("500"),
     startDate: z.string().date().optional(),
     battleType: z.string().default("8v8"),
+    osSelection: z.enum(allowedOsOptions).optional()
   }),
 );
 
@@ -40,6 +44,7 @@ const { worker } = useClientWorker<
     }),
 );
 
+const request = shallowRef<unknown>(null);
 const results = ref<BattlesProcessorResponse>();
 
 const onSubmit = form.handleSubmit((values) => {
@@ -48,22 +53,24 @@ const onSubmit = form.handleSubmit((values) => {
     title: "Form submitted!",
     description: JSON.stringify(values),
   });
-  const { map, users, limit, battleType } = form.values;
+  const { map, users, limit, battleType, osSelection } = form.values;
 
-  worker
-    .value!.request({
-      type: "battle",
-      params: {
-        battleType: battleType!,
-        map: map ?? null,
-        users: users?.flatMap((v) => v.id) ?? null,
-        limit: limit !== undefined ? parseInt(limit) : null,
-      },
-    })
-    .then((v) => {
-      console.log("worker result is ", v);
-      results.value = v;
-    });
+  const currentRequest : BattlesProcessorRequest = {
+    type: "battle",
+    params: {
+      battleType: battleType!,
+      map: map ?? null,
+      users: users?.flatMap((v) => v.id) ?? null,
+      limit: limit !== undefined ? parseInt(limit) : null,
+      afterBattle: null,
+      osSelection: osSelection ?? null
+    },
+  } as const;
+  request.value = currentRequest
+  worker.value!.request(currentRequest).then((v) => {
+    console.log("worker result is ", v);
+    if (request.value === currentRequest) results.value = v;
+  });
 });
 </script>
 
@@ -77,14 +84,26 @@ const onSubmit = form.handleSubmit((values) => {
         <legend class="mb-2 text-lg font-bold">Filter</legend>
         <GeneralMapSelector name="map"></GeneralMapSelector>
         <!-- <GeneralUserSelector name="users"></GeneralUserSelector> -->
-        <GeneralLimitsInput name="limit"></GeneralLimitsInput>
+        <GeneralRadioTabsInput
+          name="limit"
+          :values="allowedDataLimits.map((v) => ({ label: v, value: v }))"
+          label="Player limit"
+        ></GeneralRadioTabsInput>
+        <GeneralRadioTabsInput
+          name="osSelection"
+          :values="[
+            { label: 'Any', value: undefined },
+            ...allowedOsOptions.map((v) => ({ label: v, value: v }) as const),
+          ]"
+          label="Os options"
+        ></GeneralRadioTabsInput>
         <FormField v-slot="{ componentField }" name="battleType">
           <FormItem>
             <FormLabel>Battle type</FormLabel>
             <FormControl>
-              <Input type="text" placeholder="shadcn" v-bind="componentField" />
+              <Input type="text" placeholder="8v8" v-bind="componentField" />
             </FormControl>
-            <FormDescription> 8v8, 1v1, 4v4, etc... </FormDescription>
+            <FormDescription> 8v8, 1v1, 4v4 ... </FormDescription>
             <FormMessage />
           </FormItem>
         </FormField>
@@ -94,7 +113,7 @@ const onSubmit = form.handleSubmit((values) => {
         <Button type="submit" variant="outline">Update</Button>
       </form>
 
-      <div class="min-w-0 flex-1 py-10 pr-5 xl:max-w-[56rem] xl:pl-20">
+      <div class="min-w-0 flex-1 py-10 pr-5 xl:pl-20">
         <!-- <LazyGeneralClusterTest></LazyGeneralClusterTest> -->
         <template v-if="results !== undefined">
           <LazyGeneralMapPoints
@@ -110,10 +129,14 @@ const onSubmit = form.handleSubmit((values) => {
             :cluster-count="results.data.clusterCount!"
           >
           </LazyGeneralMapPoints>
-          <Tabs default-value="average-os-2" class="min-h-[600px] mt-10">
+          <Tabs default-value="average-os-2" class="mt-10 min-h-[600px]">
             <TabsList class="flex">
-              <TabsTrigger value="osdiff" class="text-base"> Os diff </TabsTrigger>
-              <TabsTrigger value="average-os" class="text-base"> Average os spikey </TabsTrigger>
+              <TabsTrigger value="osdiff" class="text-base">
+                Os diff
+              </TabsTrigger>
+              <TabsTrigger value="average-os" class="text-base">
+                Average os spikey
+              </TabsTrigger>
               <TabsTrigger value="average-os-2" class="text-base">
                 Average os smooth
               </TabsTrigger>
@@ -185,7 +208,6 @@ const onSubmit = form.handleSubmit((values) => {
               >
               </LazyGeneralOsToTimeChart>
             </TabsContent>
-
           </Tabs>
           <LazyGeneralWinrateChart :data="results.data.factionWinrate">
           </LazyGeneralWinrateChart>
