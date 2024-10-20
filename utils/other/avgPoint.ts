328ea6f229e11619bb7edcfbd9833d2e1f225f7a
Anoromi <and.zagorulko@gmail.com>
2024-10-20 14:25:56 +0300

    average point function

---
+++ utils/other/avgPoint.ts
@@ -0,0 +1,16 @@
+export function avgPoint<Point>(
+  points: Point[],
+  extractData: (value: Point) => [number, number],
+) {
+  let x = 0;
+  let y = 0;
+
+  for (const p of points) {
+    const extracted = extractData(p);
+    x += extracted[0] / points.length;
+    y += extracted[1] / points.length;
+  }
+
+  console.log("avg", x, y);
+  return [x, y];
+}
