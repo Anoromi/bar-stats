8596162e47a84e1bff137130d177ff8df06213b0
Anoromi <and.zagorulko@gmail.com>
2024-10-20 15:57:57 +0300

    add explainAnalyzeLog

--- server/utils/database/explainAnalyze.ts
+++ server/utils/database/explainAnalyze.ts
@@ -1,12 +1,17 @@
 import consola from "consola";
-import type { SQL} from "drizzle-orm";
+import type { SQL } from "drizzle-orm";
 import { sql } from "drizzle-orm";
 import type { TypedQueryBuilder } from "drizzle-orm/query-builders/query-builder";
 
-
-
 export async function explainAnalyze(query: SQL) {
   return await db.run(sql`EXPLAIN QUERY PLAN ${query}`);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function explainAnalyzeLog<T extends TypedQueryBuilder<any, any>>(
+  sql: T,
+) {
+  consola.log(await explainAnalyze(sql.getSQL()));
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
