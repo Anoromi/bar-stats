eea52119ac0fd58c36d33e498b4364edd2973e8f
Anoromi <and.zagorulko@gmail.com>
2024-10-25 20:54:29 +0300

    add sizing options to syncDatabase

--- server/api/syncDatabase.ts
+++ server/api/syncDatabase.ts
@@ -1,8 +1,27 @@
 import consola from "consola";
-import { useApplicationService } from "../utils/services/applicationService";
+import { useSyncService } from "../utils/services/syncService";
+import { z } from "zod";
 
-export default defineEventHandler(async () => {
-  console.log("hello?");
-  await useApplicationService().syncDatabase();
-  consola.log("finished");
+const querySchema = z.object({
+  highLength: z.enum(['true', 'false']).nullish().default('true')
+})
+
+export default defineEventHandler<
+{
+  query: z.infer<typeof querySchema>
+}
+>(async (event) => {
+  consola.log("start syncup");
+  const queryParams = await getValidatedQuery(event, querySchema.parse)
+  let parsePageSize: number
+  
+  if(queryParams.highLength === 'true') {
+    parsePageSize = 70
+  }
+  else {
+    parsePageSize = 20
+  }
+
+  await useSyncService().syncDatabase(parsePageSize);
+  consola.log("finished syncup");
 });
