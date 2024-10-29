319b306f44ec4a6d413d94e44f9e3524655f7e68
Anoromi <and.zagorulko@gmail.com>
2024-10-29 21:27:55 +0200

    add hints

---
+++ components/ui/hint/Hint.vue
@@ -0,0 +1,21 @@
+<script setup lang="tsx">
+import { InfoCircledIcon } from "@radix-icons/vue";
+import type { HoverCardContentProps } from "radix-vue";
+import type { HTMLAttributes } from "vue";
+
+const props = defineProps<
+  HoverCardContentProps & { class?: HTMLAttributes["class"] }
+>();
+</script>
+<template>
+  <HoverCard>
+    <HoverCardTrigger class="text-foreground-variant">
+      <Button variant="ghost" size="icon" class="font-bold">
+        <InfoCircledIcon />
+      </Button>
+    </HoverCardTrigger>
+    <HoverCardContent v-bind="props">
+      <slot />
+    </HoverCardContent>
+  </HoverCard>
+</template>
