fd6faa9791c997cbdefae4ea46e2140d866a9cf4
Anoromi <and.zagorulko@gmail.com>
2024-10-29 17:09:43 +0200

    cleanup

--- components/General/TeamWinrateChart.vue
+++ components/General/TeamWinrateChart.vue
@@ -1,5 +1,5 @@
 <script setup lang="tsx">
-import { getUniformColorRed } from '~/utils/other/uniformColor';
+import { getUniformColorRed } from "~/utils/other/uniformColor";
 
 const props = defineProps<{
   data: Record<number, number>;
@@ -37,19 +37,19 @@ const option = computed<ECOption>(() => {
     ],
   };
 });
+const { theme } = useEChartThemes();
 </script>
 <template>
   <div>
-    <h4 class="px-4 pt-2 text-xl font-bold">
-      Team win rate
-    </h4>
+    <h4 class="px-4 pt-2 text-xl font-bold">Team win rate</h4>
     <VChart
-    ref="winrate-canvas"
-    :option="option"
-    :init-options="{
-    height: 400,
-    }"
-    class="h-[400px]"
+      ref="winrate-canvas"
+      :option="option"
+      :init-options="{
+        height: 400,
+      }"
+      :theme="theme"
+      class="h-[400px]"
     ></VChart>
   </div>
 </template>
