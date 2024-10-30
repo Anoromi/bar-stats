d38c715f24733002f2cdef304bcb23e1a3d56528
Anoromi <and.zagorulko@gmail.com>
2024-10-30 13:57:58 +0200

    rename plugins directory

--- plugins/1.echart-themes.client.ts
+++ plugins/1.echart-themes.client.ts
@@ -6,6 +6,7 @@ export default defineNuxtPlugin(async () => {
     $fetch("/themes/echart-dark.json"),
   ]);
 
+  console.log('lightTheme', lightTheme)
   registerTheme("default-light", lightTheme as Record<string, string>);
   registerTheme("default-dark", darkTheme as Record<string, string>);
 });
