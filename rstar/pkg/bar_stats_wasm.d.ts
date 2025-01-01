c76342ba482a92a3030bc8989be47ddd8ff6a5ed
Anoromi <and.zagorulko@gmail.com>
2025-01-01 11:17:02 +0000

    update wasm binary

--- rstar/pkg/bar_stats_wasm.d.ts
+++ rstar/pkg/bar_stats_wasm.d.ts
@@ -1,20 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-/**
- * @param {(BarPartialPlayerData)[]} data
- * @param {number} eps
- * @param {number} min_pts
- * @param {number} _max_pts
- * @param {number} cluster_size_factor_threshold
- * @returns {DepthClusterizationResults}
- */
 export function clusterize_with_limit(data: (BarPartialPlayerData)[], eps: number, min_pts: number, _max_pts: number, cluster_size_factor_threshold: number): DepthClusterizationResults;
-/**
- * @param {Float64Array} values
- * @param {number} length
- * @param {MovingAverageOptions} method
- * @returns {Float64Array}
- */
 export function moving_average(values: Float64Array, length: number, method: MovingAverageOptions): Float64Array;
 export enum MovingAverageOptions {
   EMA = 0,
@@ -25,35 +11,75 @@ export enum MovingAverageOptions {
 }
 export class BarPartialPlayerData {
   free(): void;
-  /**
-   * @param {number} battle_index
-   * @param {number} x
-   * @param {number} y
-   */
   constructor(battle_index: number, x: number, y: number);
   battle_index: number;
   x: number;
   y: number;
 }
 export class DepthClusterizationResults {
+  private constructor();
   free(): void;
-  cluster_count: number;
   labels: Uint32Array;
+  cluster_count: number;
 }
-export class SpatialIndex {
+export class JSRtree {
   free(): void;
   constructor();
-  /**
-   * @param {number} id
-   * @param {number} x
-   * @param {number} y
-   */
   add(id: number, x: number, y: number): void;
-  /**
-   * @param {number} x
-   * @param {number} y
-   * @param {number} eps
-   * @returns {Uint32Array}
-   */
   range_query(x: number, y: number, eps: number): Uint32Array;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly __wbg_jsrtree_free: (a: number, b: number) => void;
+  readonly jsrtree_new: () => number;
+  readonly jsrtree_add: (a: number, b: number, c: number, d: number) => [number, number];
+  readonly jsrtree_range_query: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+  readonly __wbg_barpartialplayerdata_free: (a: number, b: number) => void;
+  readonly __wbg_get_barpartialplayerdata_battle_index: (a: number) => number;
+  readonly __wbg_set_barpartialplayerdata_battle_index: (a: number, b: number) => void;
+  readonly __wbg_get_barpartialplayerdata_x: (a: number) => number;
+  readonly __wbg_set_barpartialplayerdata_x: (a: number, b: number) => void;
+  readonly __wbg_get_barpartialplayerdata_y: (a: number) => number;
+  readonly __wbg_set_barpartialplayerdata_y: (a: number, b: number) => void;
+  readonly barpartialplayerdata_new: (a: number, b: number, c: number) => number;
+  readonly __wbg_depthclusterizationresults_free: (a: number, b: number) => void;
+  readonly __wbg_get_depthclusterizationresults_labels: (a: number) => [number, number];
+  readonly __wbg_set_depthclusterizationresults_labels: (a: number, b: number, c: number) => void;
+  readonly __wbg_get_depthclusterizationresults_cluster_count: (a: number) => number;
+  readonly __wbg_set_depthclusterizationresults_cluster_count: (a: number, b: number) => void;
+  readonly clusterize_with_limit: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+  readonly moving_average: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+  readonly memory: WebAssembly.Memory;
+  readonly __wbindgen_export_1: WebAssembly.Table;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_thread_destroy: (a?: number, b?: number, c?: number) => void;
+  readonly __wbindgen_start: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput, memory?: WebAssembly.Memory, thread_stack_size?: number }} module - Passing `SyncInitInput` directly is deprecated.
+* @param {WebAssembly.Memory} memory - Deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput, memory?: WebAssembly.Memory, thread_stack_size?: number } | SyncInitInput, memory?: WebAssembly.Memory): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput>, memory?: WebAssembly.Memory, thread_stack_size?: number }} module_or_path - Passing `InitInput` directly is deprecated.
+* @param {WebAssembly.Memory} memory - Deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput>, memory?: WebAssembly.Memory, thread_stack_size?: number } | InitInput | Promise<InitInput>, memory?: WebAssembly.Memory): Promise<InitOutput>;
--- rstar/pkg/bar_stats_wasm.js
+++ rstar/pkg/bar_stats_wasm.js
@@ -1,4 +1,455 @@
-import * as wasm from "./bar_stats_wasm_bg.wasm";
-export * from "./bar_stats_wasm_bg.js";
-import { __wbg_set_wasm } from "./bar_stats_wasm_bg.js";
-__wbg_set_wasm(wasm);
+let wasm;
+
+const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
+
+if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.buffer !== wasm.memory.buffer) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().slice(ptr, ptr + len));
+}
+
+function takeFromExternrefTable0(idx) {
+    const value = wasm.__wbindgen_export_1.get(idx);
+    wasm.__externref_table_dealloc(idx);
+    return value;
+}
+
+let cachedUint32ArrayMemory0 = null;
+
+function getUint32ArrayMemory0() {
+    if (cachedUint32ArrayMemory0 === null || cachedUint32ArrayMemory0.buffer !== wasm.memory.buffer) {
+        cachedUint32ArrayMemory0 = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachedUint32ArrayMemory0;
+}
+
+function getArrayU32FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getUint32ArrayMemory0().subarray(ptr / 4, ptr / 4 + len);
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray32ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 4, 4) >>> 0;
+    getUint32ArrayMemory0().set(arg, ptr / 4);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachedDataViewMemory0 = null;
+
+function getDataViewMemory0() {
+    if (cachedDataViewMemory0 === null || cachedDataViewMemory0.buffer !== wasm.memory.buffer) {
+        cachedDataViewMemory0 = new DataView(wasm.memory.buffer);
+    }
+    return cachedDataViewMemory0;
+}
+
+function addToExternrefTable0(obj) {
+    const idx = wasm.__externref_table_alloc();
+    wasm.__wbindgen_export_1.set(idx, obj);
+    return idx;
+}
+
+function passArrayJsValueToWasm0(array, malloc) {
+    const ptr = malloc(array.length * 4, 4) >>> 0;
+    const mem = getDataViewMemory0();
+    for (let i = 0; i < array.length; i++) {
+        mem.setUint32(ptr + 4 * i, addToExternrefTable0(array[i]), true);
+    }
+    WASM_VECTOR_LEN = array.length;
+    return ptr;
+}
+/**
+ * @param {(BarPartialPlayerData)[]} data
+ * @param {number} eps
+ * @param {number} min_pts
+ * @param {number} _max_pts
+ * @param {number} cluster_size_factor_threshold
+ * @returns {DepthClusterizationResults}
+ */
+export function clusterize_with_limit(data, eps, min_pts, _max_pts, cluster_size_factor_threshold) {
+    const ptr0 = passArrayJsValueToWasm0(data, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.clusterize_with_limit(ptr0, len0, eps, min_pts, _max_pts, cluster_size_factor_threshold);
+    return DepthClusterizationResults.__wrap(ret);
+}
+
+let cachedFloat64ArrayMemory0 = null;
+
+function getFloat64ArrayMemory0() {
+    if (cachedFloat64ArrayMemory0 === null || cachedFloat64ArrayMemory0.buffer !== wasm.memory.buffer) {
+        cachedFloat64ArrayMemory0 = new Float64Array(wasm.memory.buffer);
+    }
+    return cachedFloat64ArrayMemory0;
+}
+
+function passArrayF64ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 8, 8) >>> 0;
+    getFloat64ArrayMemory0().set(arg, ptr / 8);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+function getArrayF64FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getFloat64ArrayMemory0().subarray(ptr / 8, ptr / 8 + len);
+}
+/**
+ * @param {Float64Array} values
+ * @param {number} length
+ * @param {MovingAverageOptions} method
+ * @returns {Float64Array}
+ */
+export function moving_average(values, length, method) {
+    const ptr0 = passArrayF64ToWasm0(values, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.moving_average(ptr0, len0, length, method);
+    if (ret[3]) {
+        throw takeFromExternrefTable0(ret[2]);
+    }
+    var v2 = getArrayF64FromWasm0(ret[0], ret[1]).slice();
+    wasm.__wbindgen_free(ret[0], ret[1] * 8, 8);
+    return v2;
+}
+
+/**
+ * @enum {0 | 1 | 2 | 3 | 4}
+ */
+export const MovingAverageOptions = Object.freeze({
+    EMA: 0, "0": "EMA",
+    SMA: 1, "1": "SMA",
+    SMM: 2, "2": "SMM",
+    WSMA: 3, "3": "WSMA",
+    SSMA: 4, "4": "SSMA",
+});
+
+const BarPartialPlayerDataFinalization = (typeof FinalizationRegistry === 'undefined')
+    ? { register: () => {}, unregister: () => {} }
+    : new FinalizationRegistry(ptr => wasm.__wbg_barpartialplayerdata_free(ptr >>> 0, 1));
+
+export class BarPartialPlayerData {
+
+    static __unwrap(jsValue) {
+        if (!(jsValue instanceof BarPartialPlayerData)) {
+            return 0;
+        }
+        return jsValue.__destroy_into_raw();
+    }
+
+    __destroy_into_raw() {
+        const ptr = this.__wbg_ptr;
+        this.__wbg_ptr = 0;
+        BarPartialPlayerDataFinalization.unregister(this);
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        wasm.__wbg_barpartialplayerdata_free(ptr, 0);
+    }
+    /**
+     * @returns {number}
+     */
+    get battle_index() {
+        const ret = wasm.__wbg_get_barpartialplayerdata_battle_index(this.__wbg_ptr);
+        return ret >>> 0;
+    }
+    /**
+     * @param {number} arg0
+     */
+    set battle_index(arg0) {
+        wasm.__wbg_set_barpartialplayerdata_battle_index(this.__wbg_ptr, arg0);
+    }
+    /**
+     * @returns {number}
+     */
+    get x() {
+        const ret = wasm.__wbg_get_barpartialplayerdata_x(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+     * @param {number} arg0
+     */
+    set x(arg0) {
+        wasm.__wbg_set_barpartialplayerdata_x(this.__wbg_ptr, arg0);
+    }
+    /**
+     * @returns {number}
+     */
+    get y() {
+        const ret = wasm.__wbg_get_barpartialplayerdata_y(this.__wbg_ptr);
+        return ret;
+    }
+    /**
+     * @param {number} arg0
+     */
+    set y(arg0) {
+        wasm.__wbg_set_barpartialplayerdata_y(this.__wbg_ptr, arg0);
+    }
+    /**
+     * @param {number} battle_index
+     * @param {number} x
+     * @param {number} y
+     */
+    constructor(battle_index, x, y) {
+        const ret = wasm.barpartialplayerdata_new(battle_index, x, y);
+        this.__wbg_ptr = ret >>> 0;
+        BarPartialPlayerDataFinalization.register(this, this.__wbg_ptr, this);
+        return this;
+    }
+}
+
+const DepthClusterizationResultsFinalization = (typeof FinalizationRegistry === 'undefined')
+    ? { register: () => {}, unregister: () => {} }
+    : new FinalizationRegistry(ptr => wasm.__wbg_depthclusterizationresults_free(ptr >>> 0, 1));
+
+export class DepthClusterizationResults {
+
+    static __wrap(ptr) {
+        ptr = ptr >>> 0;
+        const obj = Object.create(DepthClusterizationResults.prototype);
+        obj.__wbg_ptr = ptr;
+        DepthClusterizationResultsFinalization.register(obj, obj.__wbg_ptr, obj);
+        return obj;
+    }
+
+    __destroy_into_raw() {
+        const ptr = this.__wbg_ptr;
+        this.__wbg_ptr = 0;
+        DepthClusterizationResultsFinalization.unregister(this);
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        wasm.__wbg_depthclusterizationresults_free(ptr, 0);
+    }
+    /**
+     * @returns {Uint32Array}
+     */
+    get labels() {
+        const ret = wasm.__wbg_get_depthclusterizationresults_labels(this.__wbg_ptr);
+        var v1 = getArrayU32FromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
+        return v1;
+    }
+    /**
+     * @param {Uint32Array} arg0
+     */
+    set labels(arg0) {
+        const ptr0 = passArray32ToWasm0(arg0, wasm.__wbindgen_malloc);
+        const len0 = WASM_VECTOR_LEN;
+        wasm.__wbg_set_depthclusterizationresults_labels(this.__wbg_ptr, ptr0, len0);
+    }
+    /**
+     * @returns {number}
+     */
+    get cluster_count() {
+        const ret = wasm.__wbg_get_depthclusterizationresults_cluster_count(this.__wbg_ptr);
+        return ret >>> 0;
+    }
+    /**
+     * @param {number} arg0
+     */
+    set cluster_count(arg0) {
+        wasm.__wbg_set_depthclusterizationresults_cluster_count(this.__wbg_ptr, arg0);
+    }
+}
+
+const JSRtreeFinalization = (typeof FinalizationRegistry === 'undefined')
+    ? { register: () => {}, unregister: () => {} }
+    : new FinalizationRegistry(ptr => wasm.__wbg_jsrtree_free(ptr >>> 0, 1));
+
+export class JSRtree {
+
+    __destroy_into_raw() {
+        const ptr = this.__wbg_ptr;
+        this.__wbg_ptr = 0;
+        JSRtreeFinalization.unregister(this);
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        wasm.__wbg_jsrtree_free(ptr, 0);
+    }
+    constructor() {
+        const ret = wasm.jsrtree_new();
+        this.__wbg_ptr = ret >>> 0;
+        JSRtreeFinalization.register(this, this.__wbg_ptr, this);
+        return this;
+    }
+    /**
+     * @param {number} id
+     * @param {number} x
+     * @param {number} y
+     */
+    add(id, x, y) {
+        const ret = wasm.jsrtree_add(this.__wbg_ptr, id, x, y);
+        if (ret[1]) {
+            throw takeFromExternrefTable0(ret[0]);
+        }
+    }
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param {number} eps
+     * @returns {Uint32Array}
+     */
+    range_query(x, y, eps) {
+        const ret = wasm.jsrtree_range_query(this.__wbg_ptr, x, y, eps);
+        if (ret[3]) {
+            throw takeFromExternrefTable0(ret[2]);
+        }
+        var v1 = getArrayU32FromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
+        return v1;
+    }
+}
+
+async function __wbg_load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+function __wbg_get_imports() {
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbg_barpartialplayerdata_unwrap = function(arg0) {
+        const ret = BarPartialPlayerData.__unwrap(arg0);
+        return ret;
+    };
+    imports.wbg.__wbg_log_1368ba8d2340558d = function(arg0, arg1) {
+        console.log(getStringFromWasm0(arg0, arg1));
+    };
+    imports.wbg.__wbindgen_init_externref_table = function() {
+        const table = wasm.__wbindgen_export_1;
+        const offset = table.grow(4);
+        table.set(0, undefined);
+        table.set(offset + 0, undefined);
+        table.set(offset + 1, null);
+        table.set(offset + 2, true);
+        table.set(offset + 3, false);
+        ;
+    };
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        const ret = getStringFromWasm0(arg0, arg1);
+        return ret;
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    return imports;
+}
+
+function __wbg_init_memory(imports, memory) {
+    imports.wbg.memory = memory || new WebAssembly.Memory({initial:18,maximum:16384,shared:true});
+}
+
+function __wbg_finalize_init(instance, module, thread_stack_size) {
+    wasm = instance.exports;
+    __wbg_init.__wbindgen_wasm_module = module;
+    cachedDataViewMemory0 = null;
+    cachedFloat64ArrayMemory0 = null;
+    cachedUint32ArrayMemory0 = null;
+    cachedUint8ArrayMemory0 = null;
+
+    if (typeof thread_stack_size !== 'undefined' && (typeof thread_stack_size !== 'number' || thread_stack_size === 0 || thread_stack_size % 65536 !== 0)) { throw 'invalid stack size' }
+    wasm.__wbindgen_start(thread_stack_size);
+    return wasm;
+}
+
+function initSync(module, memory) {
+    if (wasm !== undefined) return wasm;
+
+    let thread_stack_size
+    if (typeof module !== 'undefined') {
+        if (Object.getPrototypeOf(module) === Object.prototype) {
+            ({module, memory, thread_stack_size} = module)
+        } else {
+            console.warn('using deprecated parameters for `initSync()`; pass a single object instead')
+        }
+    }
+
+    const imports = __wbg_get_imports();
+
+    __wbg_init_memory(imports, memory);
+
+    if (!(module instanceof WebAssembly.Module)) {
+        module = new WebAssembly.Module(module);
+    }
+
+    const instance = new WebAssembly.Instance(module, imports);
+
+    return __wbg_finalize_init(instance, module, thread_stack_size);
+}
+
+async function __wbg_init(module_or_path, memory) {
+    if (wasm !== undefined) return wasm;
+
+    let thread_stack_size
+    if (typeof module_or_path !== 'undefined') {
+        if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
+            ({module_or_path, memory, thread_stack_size} = module_or_path)
+        } else {
+            console.warn('using deprecated parameters for the initialization function; pass a single object instead')
+        }
+    }
+
+    if (typeof module_or_path === 'undefined') {
+        module_or_path = new URL('bar_stats_wasm_bg.wasm', import.meta.url);
+    }
+    const imports = __wbg_get_imports();
+
+    if (typeof module_or_path === 'string' || (typeof Request === 'function' && module_or_path instanceof Request) || (typeof URL === 'function' && module_or_path instanceof URL)) {
+        module_or_path = fetch(module_or_path);
+    }
+
+    __wbg_init_memory(imports, memory);
+
+    const { instance, module } = await __wbg_load(await module_or_path, imports);
+
+    return __wbg_finalize_init(instance, module, thread_stack_size);
+}
+
+export { initSync };
+export default __wbg_init;
